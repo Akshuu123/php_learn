1f5d88d2b5a41cb830ed9c79d6cbb8871120b21c
Akshuu123 <akshaychauhan15397@gmail.com>
2024-03-28 09:50:13 +0530

    add str replace

--- Strings/str_replace.php
+++ Strings/str_replace.php
@@ -1,10 +1,11 @@
 <?php
 include 'strings.php';
+// argument(removeitem ,needle ,haystack)
 // strreplace (out,in,str)
 
 // echo $str;
 // echo str_replace('a','f',$str);
 
 
-echo $lorem;
-echo str_replace("Pariatur","akshay",$lorem);
+echo $lorem."\n";
+echo str_replace("Pariatur", "akshay", $lorem) ."\n";
